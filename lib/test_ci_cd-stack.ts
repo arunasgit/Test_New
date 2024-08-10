27c56dfdf7343045266fea369bd8e06804ff46fb
arunasgit <asarun6@gmail.com>
2024-08-10 00:28:50 -0400

    initial commit

--- lib/test_ci_cd-stack.ts
+++ lib/test_ci_cd-stack.ts
@@ -11,7 +11,7 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('arunasgit/test_firstcode', 'main'), //Remember to change 
+        input: CodePipelineSource.gitHub('arunasgit/Test_New', 'main'), //Remember to change 
         commands: ['npm ci', 
                    'npm run build', 
                    'npx cdk synth']
